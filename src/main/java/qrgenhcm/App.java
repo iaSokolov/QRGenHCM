451c23ab944dcdcaea2cfec9b362eed53dd71003
iaSokolov <iasokolov@gmail.com>
2021-01-31 09:58:39 +0300

    version 1

--- src/main/java/qrgenhcm/App.java
+++ src/main/java/qrgenhcm/App.java
@@ -1,5 +1,38 @@
 package qrgenhcm;
 
-public class App {
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class App {
+    private static void generateQRCodeImage(String text, int width, int height, String filePath)
+            throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Error import parameter");
+        } else {
+            try {
+                String text = args[0];
+                String filePath = args[1];
+                generateQRCodeImage(text, 350, 350, filePath);
+            } catch (WriterException e) {
+                System.out.println("Could not generate QR Code, WriterException :: " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("Could not generate QR Code, IOException :: " + e.getMessage());
+            }
+        }
+    }
 }
